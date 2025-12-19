9760cb9d0aa579ee48b95b153533c0118be0398c
Amr Ali <20241701724@cis.asu.edu.eg>
2025-12-20 01:51:37 +0200

    Delete backend/models/CustomerUser.js

--- backend/models/CustomerUser.js
+++
@@ -1,5 +0,0 @@
-// Customer users are stored in the User collection with role: "customer"
-// This file is deprecated - use User.js instead
-const User = require("./User");
-
-module.exports = User;
